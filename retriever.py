3692fa8993efb027cf9012e2c4bd6657b10866dd
suhaas <suhaas.teja@gmail.com>
2025-03-29 18:16:29 -0700

    basic streamlit UI

--- retriever.py
+++ retriever.py
@@ -1,7 +1,8 @@
 from dotenv import load_dotenv
 import os
-from langchain_openai import OpenAIEmbeddings
-from langchain_chroma import Chroma
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+import streamlit as st
 
 load_dotenv()
 
@@ -13,9 +14,19 @@ vector_store = Chroma(
     persist_directory="./chroma_langchain_db",
 )
 
-query = "What's the Quiller?"
-results = vector_store.similarity_search_with_relevance_scores(query, k=3)  # Get top 3 matches
+def retrieve_results(query):
+    print(query)
+    # query = "What's the Quiller?"
+    results = vector_store.similarity_search_with_relevance_scores(query, k=10)  # Get top 3 matches
 
 
-for doc, score in results:
-    print(f"Score: {score}\n Url: {doc.metadata['url']}\nTitle: {doc.metadata['title']}\nContent: {doc.page_content[:20]}\n")
+    for doc, score in results:
+        st.write(f"Score: {score}\n Url: {doc.metadata['url']}\nTitle: {doc.metadata['title']}\nContent: {doc.page_content[:20]}\n")
+        print(f"Score: {score}\n Url: {doc.metadata['url']}\nTitle: {doc.metadata['title']}\nContent: {doc.page_content[:20]}\n")
+
+
+query = st.text_input("Ask a question:")
+
+# Trigger results retrieval when a query is entered
+if query:
+    retrieve_results(query)
