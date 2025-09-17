9b571316c40028cd172c87096ac00fda6205e88b
Suhaas <63154219+suhaasteja@users.noreply.github.com>
2025-09-16 18:29:54 -0700

    Update retriever.py

--- retriever.py
+++ retriever.py
@@ -1,6 +1,6 @@
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+# __import__('pysqlite3')
+# import sys
+# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 from dotenv import load_dotenv
 import os
 from langchain_community.embeddings import OpenAIEmbeddings
